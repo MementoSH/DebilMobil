c6b62c6729ab4c75dd71ab15f5ed5e7a1fbcca7f
m1crosofe <masnyuk2000@mail.ru>
2024-04-15 18:06:46 +0500

    popa

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,35 @@
+<div class="header mx-auto container flex space-between">
+    <img src="" alt="">
+    <a href="/">DebilMobil</a>
+    <span>Ride</span>
+    <span>Rent</span>
+    <span>Eat</span>
+    <span>Hotels</span>
+    <div class="reg">
+        <a href="">Sign up</a>
+    </div>
+    <div class="log">
+        <a href="">Log in</a>
+    </div>
+</div>
+<hr/>
+<style>
+    .header{
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        color: white;
+        padding-top: 20px;
+        font-size: large;
+    }
+    .reg{
+        border-radius: 12px;
+        background: #1A69E5;
+        padding: 8px;
+    }
+    .log{
+        border-radius: 12px;
+        background: #292E38;
+        padding: 10px;
+    }
+</style>
