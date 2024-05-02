63301dc0b47bc2deb8513522d2365da324dd7311
Роман Хмызенко <145050746+owennz1499@users.noreply.github.com>
2024-05-02 20:23:55 +0500

    global param

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,20 @@
     import Header from "$lib/components/Header.svelte";
     import "../app.css";
 </script>
+<body>
 <div class="jopa">   
     <Header/>
     <Footer/>
 </div>
-
+</body>
 <style>
     .jopa{
         background-color: black;
-    }    
+        
+    }   
+    :global(body) {
+      gap: 0;
+      margin: 0;
+      overflow-y: hidden;
+    } 
 </style>
